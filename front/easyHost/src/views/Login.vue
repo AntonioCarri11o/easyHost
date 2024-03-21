1238f4f913174ece0ad6fdc46ec8bc2f1af7a0c5
Yahir Alberto <136031489+yahirdev13@users.noreply.github.com>
2024-03-21 12:02:01 -0600

    [ADD] validaciones de los modals, no estan terminados al 100%

--- front/easyHost/src/views/Login.vue
+++ front/easyHost/src/views/Login.vue
@@ -49,11 +49,16 @@
     </b-container>
 
     <div>
-
       <b-modal id="modal-recuperar" centered title="Recuperar Contraseña" hide-footer no-stacking>
         <form>
           <label class="mb-2">Ingresa tu número telefonico (10 digitos)</label>
           <b-form-input class="input-modal"></b-form-input>
+          <b-form-group id="fieldset-1" description="Ingresa tu número de 10 digitos" label="Número de Teléfono"
+            label-for="input-phone" valid-feedback="Número válido" :invalid-feedback="phoneInvalidFeedback"
+            :state="phoneState">
+            <b-form-input id="input-phone" v-model="phoneNumber" :state="phoneState" @input="validatePhoneNumber"
+              type="tel"></b-form-input>
+          </b-form-group>
           <b-button class="btn-modal mt-4" @click="$bvModal.hide('modal-recuperar')">Cancelar</b-button>
           <b-button class="btn-modal mt-4 ms-2" variant="primary" v-b-modal.modal-codigo>Continuar</b-button>
         </form>
@@ -61,13 +66,48 @@
     </div>
 
     <div>
-
       <b-modal id="modal-codigo" centered title="Recuperar Contraseña" hide-footer no-stacking>
         <form>
           <label class="mb-2">Ingresa el código de verificación (5 digitos)</label>
           <b-form-input autocomplete="off" class="input-modal"></b-form-input>
+          <b-form-group id="fieldset-2" description="Enter the 5-digit verification code." label="Verification Code"
+            label-for="input-code" valid-feedback="Thank you!" :invalid-feedback="codeInvalidFeedback"
+            :state="codeState">
+            <b-form-input id="input-code" v-model="verificationCode" :state="codeState"
+              @input="validateVerificationCode"></b-form-input>
+          </b-form-group>
           <b-button class="btn-modal mt-4" @click="$bvModal.hide('modal-codigo')">Cancelar</b-button>
-          <b-button class="btn-modal mt-4 ms-2" variant="primary" v-b-modal.modal-codigo>Continuar</b-button>
+          <b-button class="btn-modal mt-4 ms-2" variant="primary" v-b-modal.modal-contrasenias>Continuar</b-button>
+        </form>
+      </b-modal>
+    </div>
+
+    <div>
+      <b-modal id="modal-contrasenias" centered title="Recuperar Contraseña" hide-footer no-stacking>
+        <form>
+          <label class="mb-2">Ingresa tu nueva contraseña</label>
+          <b-form-input autocomplete="off"></b-form-input>
+          <label class="mb-2 mt-4">Confirma tu nueva contraseña</label>
+          <b-form-input autocomplete="off"></b-form-input>
+
+          <!-- Input para la contraseña -->
+          <b-form-group id="fieldset-3"
+            description="Enter a password with at least 8 characters, one uppercase letter, one number, and one special character."
+            label="Password" label-for="input-password" valid-feedback="Thank you!"
+            :invalid-feedback="passwordInvalidFeedback" :state="passwordState">
+            <b-form-input type="password" id="input-password" v-model="password" :state="passwordState"
+              @input="validatePassword"></b-form-input>
+          </b-form-group>
+
+          <!-- Input para confirmar la contraseña -->
+          <b-form-group id="fieldset-4" description="Confirm your password." label="Confirm Password"
+            label-for="input-confirm-password" valid-feedback="Passwords match!"
+            :invalid-feedback="confirmPasswordInvalidFeedback" :state="confirmPasswordState">
+            <b-form-input type="password" id="input-confirm-password" v-model="confirmPassword"
+              :state="confirmPasswordState" @input="validateConfirmPassword"></b-form-input>
+          </b-form-group>
+          <b-button class="btn-modal mt-4" @click="$bvModal.hide('modal-contrasenias')">Cancelar</b-button>
+          <b-button class="btn-modal mt-4 ms-2" variant="primary">Confirmar</b-button>
         </form>
       </b-modal>
     </div>
@@ -95,6 +135,12 @@ export default {
       email: "",
       contrasenia: "",
       enviado: false,
+
+
+      phoneNumber: '',
+      verificationCode: '',
+      password: '',
+      confirmPassword: ''
     };
   },
   validations() {
@@ -106,11 +152,54 @@ export default {
       },
     };
   },
+  computed: {
+    phoneState() {
+      return /^\d{10}$/.test(this.phoneNumber)
+    },
+    phoneInvalidFeedback() {
+      return this.phoneState ? '' : 'Por favor ingresa un número de 10 dígitos.'
+    },
+    codeState() {
+      return /^\d{5}$/.test(this.verificationCode)
+    },
+    codeInvalidFeedback() {
+      return this.codeState ? '' : 'Please enter a 5-digit verification code.'
+    },
+    passwordState() {
+      return /^(?=.*[A-Z])(?=.*\d)(?=.*[!@#$%^&*()_+])[A-Za-z\d!@#$%^&*()_+]{8,}$/.test(this.password)
+    },
+    passwordInvalidFeedback() {
+      return this.passwordState ? '' : 'Password must be at least 8 characters long and contain at least one uppercase letter, one number, and one special character.'
+    },
+    confirmPasswordState() {
+      return this.confirmPassword === this.password
+    },
+    confirmPasswordInvalidFeedback() {
+      return this.confirmPasswordState ? '' : 'Passwords do not match.'
+    }
+  },
   methods: {
     validar() {
       this.enviado = true;
       // Si los datos son válidos, se envían al servidor
     },
+    validatePhoneNumber() {
+      // Elimina caracteres no numéricos del número de teléfono
+      this.phoneNumber = this.phoneNumber.replace(/\D/g, '').slice(0, 10);
+    },
+    validateVerificationCode() {
+      // Elimina caracteres no numéricos del código de verificación
+      this.verificationCode = this.verificationCode.replace(/\D/g, '').slice(0, 5);
+    },
+    validatePassword() {
+      // No permitir espacios en blanco en la contraseña
+      this.password = this.password.replace(/\s/g, '')
+    },
+    validateConfirmPassword() {
+      // No permitir espacios en blanco en la confirmación de contraseña
+      this.confirmPassword = this.confirmPassword.replace(/\s/g, '')
+    },
+
   },
 };
 </script>
