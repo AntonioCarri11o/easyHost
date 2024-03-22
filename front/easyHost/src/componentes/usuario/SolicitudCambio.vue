acd625ff110f3f08b3f311aaa9c457a02eada658
Joviicam Racoon <105244358+joviicam@users.noreply.github.com>
2024-03-22 09:58:24 -0600

    Hola

--- front/easyHost/src/componentes/usuario/SolicitudCambio.vue
+++ front/easyHost/src/componentes/usuario/SolicitudCambio.vue
@@ -1,6 +1,6 @@
 <template>
     <b-container fluid class="d-block min-vh-100 flex-column align-items-center">
-        <div style="max-width: 1200px; width: 100%;">
+        <div>
             <h2 class="text-center mt-5 mb-4">Solicitar ser anfitrión</h2>
             <hr class="w-100">
             <div class="alert alert-warning mt-3 mb-4 alert-dismissible fade show" role="alert">
@@ -20,9 +20,11 @@
                 </b-form-group>
                 <!-- Imagen de la identificación-->
                 <b-form-group id="input-group-3" label="Identificación oficial:" label-for="input-3" class="mb-3">
-                    <b-form-file v-model="form.url_imagen_identificacion_anfitrion" type="file" accept="image/*"
-                        placeholder="Seleccione una imagen" required></b-form-file>
-                    
+                    <b-icon icon="arrow-bar-up" class="mr-2"></b-icon>
+                    <b-form-file v-model="form.url_imagen_identificacion_anfitrion" @change="previewImage" type="file"
+                        accept="image/*" placeholder="Seleccione una imagen" required></b-form-file>
+                    <img v-if="imageUrl" :src="imageUrl" alt="Vista previa de la imagen"
+                        style="max-width: 100%; margin-top: 10px;">
                 </b-form-group>
 
                 <b-row class="justify-content-center">
@@ -33,11 +35,26 @@
                         <b-button type="reset" variant="danger">Limpiar</b-button>
                     </b-col>
                     <b-col cols="auto" class="mb-3">
-                        <b-button type="submit" variant="primary">Enviar</b-button>
+                        <b-button type="submit" variant="primary" v-b-modal.modal-AvisoPrivacidad>Enviar</b-button>
                     </b-col>
                 </b-row>
             </b-form>
         </div>
+        <b-modal id="modal-AvisoPrivacidad" title="AVISO DE PRIVACIDAD" modal-ok-title="Aceptar" @ok="enviar()"
+            size="lg" hide-header-close>
+            <p class="my-3" style="text-align: justify;">Te informamos que al registrarte como anfitrión en nuestra
+                aplicación, necesitamos recopilar cierta información personal para validar y aprobar tu solicitud. Este
+                proceso es crucial para garantizar la seguridad y la calidad de la plataforma.<br> <br>
+
+                Consentimiento: Al solicitar ser anfitrión y proporcionar la información requerida, aceptas expresamente
+                los términos y condiciones de este aviso de privacidad.<br><br>
+
+                Acceso y Modificación: Tienes derecho a acceder, rectificar y cancelar tus datos personales. Puedes
+                realizar estas acciones a través de la sección correspondiente en tu perfil de usuario.<br><br>
+
+                Agradecemos tu comprensión y colaboración para garantizar la integridad y la confianza en nuestra
+                comunidad de anfitriones y huéspedes.</p>
+        </b-modal>
     </b-container>
 </template>
 
@@ -49,7 +66,8 @@
                     curp_anfitrion: '',
                     rfc_anfitrion: '',
                     url_imagen_identificacion_anfitrion: null
-                }
+                },
+                imageUrl: null
             }
         },
         methods: {
@@ -62,9 +80,21 @@
                 this.form.curp_anfitrion = '';
                 this.form.rfc_anfitrion = '';
                 this.form.url_imagen_identificacion_anfitrion = null;
+                this.imageUrl = null;
+            
                 this.$nextTick(() => {
                     this.$refs['form'].reset();
                 });
+            },
+            previewImage(event) {
+                const file = event.target.files[0];
+                if (file) {
+                    this.imageUrl = URL.createObjectURL(file);
+                }
+            },
+            enviar() {
+                alert('Solicitud enviada');
+                this.$router.push({ name: 'Ejemplo' });
             }
         }
     }
